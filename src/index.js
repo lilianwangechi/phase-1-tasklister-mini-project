c32d3eb06048936fbd4e02f3e5bd6eb000f6ec4d
lilian <lilian.wangechi@student.moringaschool.com>
2022-08-19 08:51:15 +0300

    basic assignment requirements completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+ //console.log('After dom loaded')
+ //user should be able to click a submit button
+ document.querySelector('form').addEventListener('submit',(e)=>{
+  e.preventDefault()
+  handleToDo(e.target.new_task.value)
+  //console.log(e)
+ })
+})
+
+//the task added by the user should appear in the dom..append
+function handleToDo(todo){
+  let li = document.createElement('li')
+  li.textContent = todo
+  document.querySelector("#tasks").appendChild(li)
+  //console.log(p)
+}
